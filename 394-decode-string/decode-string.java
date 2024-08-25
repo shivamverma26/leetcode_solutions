89b94b30110638ebb8d6f8050ad11a5220694a21
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2024-08-26 03:00:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (36.52%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,29 @@
+class Solution {
+   int i = 0;
+public String decodeString(String s) {
+    StringBuilder sb = new StringBuilder();
+    int count = 0;
+    String tmp_string = "";
+    
+    while (i < s.length()) {
+        char c = s.charAt(i);
+        i++;
+        
+        if (c == '[') {
+            tmp_string = decodeString(s); // do subproblem
+            for (int j = 0; j < count; j++) {
+                sb.append(tmp_string);
+            }
+            count = 0; // reset counter
+        } else if (c == ']') { // subproblem complete
+            break;
+        } else if (Character.isAlphabetic(c)) {
+            sb.append(c);
+        } else {
+            count = count * 10 + c - '0';
+        }
+    }
+    
+    return sb.toString();
+}
+}

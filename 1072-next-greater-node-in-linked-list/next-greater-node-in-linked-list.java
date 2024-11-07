3651e0ad78a26cd830540ad5542076b90d5825b4
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2024-11-07 19:48:50 +0530

    Time: 16 ms (96.37%) | Memory: 47.4 MB (55.94%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
@@ -1,58 +1,36 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    // public ListNode reverse(ListNode head) {
-    // ListNode curr = head, prev = null, next;
-
-    // while (curr != null) {
-    // next = curr.next;
-    // curr.next = prev;
-    // prev = curr;
-    // curr = next;
-    // }
-    // return prev;
-
-    // }
-
     public int[] nextLargerNodes(ListNode head) {
-        int count = 0;
-
         ListNode temp = head;
+        int count = 0;
         while (temp != null) {
-            temp = temp.next;
             count++;
+            temp = temp.next;
         }
-
-        int arr[] = new int[count];
-        int ans[] = new int[count];
-        int i = 0;
-
-        while (head != null) {
-            arr[i++] = head.val;
-            head = head.next;
+        int[] arr = new int[count];
+        int[] result = new int[count];
+        temp = head;
+        for (int i = 0; i < count; i++) {
+            arr[i] = temp.val;
+            temp = temp.next;
         }
-
-        i = 0;
-        for (int j = 0; j < count; j++) {
-            ans[i] = 0;
-            for (int k = j + 1; k < count; k++) {
-                if (arr[k] > arr[j]) {
-                    ans[i] = arr[k];
-                    break;
-                }
-
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < count; i++) {
+            while (!stack.isEmpty() && arr[i] > arr[stack.peek()]) {
+                int index = stack.pop();
+                result[index] = arr[i]; 
             }
-            i++;
+            stack.push(i); 
         }
-        return ans;
-
+        return result;
     }
 }

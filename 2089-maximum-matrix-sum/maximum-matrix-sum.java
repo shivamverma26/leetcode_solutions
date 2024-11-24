29d6d7237e66b09baf3964a0dfa95ece5ce487b0
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2024-11-24 22:22:23 +0530

    Time: 7 ms (29.87%) | Memory: 55 MB (62.44%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+        long sum = 0, mini = Long.MAX_VALUE;
+        int n = matrix.length, cnt = 0;
+        
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (matrix[row][col] < 0) {
+                    cnt++;
+                }
+                mini = Math.min(Math.abs(matrix[row][col]), mini);
+                sum += Math.abs(matrix[row][col]);
+            }
+        }
+        
+        if ((cnt & 1) == 1) {
+            sum -= (2 * mini);
+        }
+        return sum;
+    }
+}
